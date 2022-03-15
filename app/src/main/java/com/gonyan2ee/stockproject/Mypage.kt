5af873fb348bbdbe06745bc5b282a9d3fa560d9f
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-03-15 17:40:35 +0900

    Login XML_revision

---
+++ app/src/main/java/com/gonyan2ee/stockproject/Mypage.kt
@@ -0,0 +1,11 @@
+package com.gonyan2ee.stockproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mypage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage)
+    }
+}
