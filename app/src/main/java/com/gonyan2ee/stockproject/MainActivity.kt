9973ae35e5a84280d128d38cef4bea6fc88382b2
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-03-16 18:13:06 +0900

    bottom Navigation

--- app/src/main/java/com/gonyan2ee/stockproject/MainActivity.kt
+++ app/src/main/java/com/gonyan2ee/stockproject/MainActivity.kt
@@ -2,10 +2,16 @@ package com.gonyan2ee.stockproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gonyan2ee.stockproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.bottomNavigation.setOnNavigationItemReselectedListener() {
+
+        }
     }
 }
