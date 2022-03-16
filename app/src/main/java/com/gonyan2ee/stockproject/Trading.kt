81b4b3309665140719c96ec14efcfb1bfabcdfbc
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-03-16 14:18:25 +0900

    Coroutine

--- app/src/main/java/com/gonyan2ee/stockproject/Trading.kt
+++ app/src/main/java/com/gonyan2ee/stockproject/Trading.kt
@@ -1,12 +1,8 @@
 package com.gonyan2ee.stockproject
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
@@ -18,42 +14,40 @@ class Trading : AppCompatActivity() {
         val kospi = mutableMapOf<String, Int>()
 
         CoroutineScope(Dispatchers.Main).launch {
-            kospi["Samsung"] = sharePrice("https://finance.naver.com/item/main.naver?code=005930")
-            kospi["LGEnergySolution"] = sharePrice("https://finance.naver.com/item/main.naver?code=373220")
-            kospi["Samsung"] = sharePrice("https://finance.naver.com/item/main.naver?code=005930")
-            kospi["LGEnergySolution"] = sharePrice("https://finance.naver.com/item/main.naver?code=373220")
-            kospi["SKHynix"] = sharePrice("https://finance.naver.com/item/main.naver?code=000660")
-            kospi["NAVER"] = sharePrice("https://finance.naver.com/item/main.naver?code=035420")
-            kospi["SamsungBiologics"] = sharePrice("https://finance.naver.com/item/main.naver?code=207940")
-            kospi["KaKao"] = sharePrice("https://finance.naver.com/item/main.naver?code=035720")
-            kospi["Hyundai"] = sharePrice("https://finance.naver.com/item/main.naver?code=005380")
-            kospi["SamsungSDI"] = sharePrice("https://finance.naver.com/item/main.naver?code=006400")
-            kospi["LGChemistry"] = sharePrice("https://finance.naver.com/item/main.naver?code=051910")
-            kospi["Kia"] = sharePrice("https://finance.naver.com/item/main.naver?code=000270")
-            kospi["KakaoBank"] = sharePrice("https://finance.naver.com/item/main.naver?code=323410")
-            kospi["Celltrion"] = sharePrice("https://finance.naver.com/item/main.naver?code=068270")
-            kospi["POSCO"] = sharePrice("https://finance.naver.com/item/main.naver?code=005490")
-            kospi["KBFinancialGroup"] = sharePrice("https://finance.naver.com/item/main.naver?code=105560")
-            kospi["SamsungCnT"] = sharePrice("https://finance.naver.com/item/main.naver?code=028260")
-            kospi["LGElectronics"] = sharePrice("https://finance.naver.com/item/main.naver?code=066570")
-            kospi["ShinhanHoldings"] = sharePrice("https://finance.naver.com/item/main.naver?code=055550")
-            kospi["HyundaiMobis"] = sharePrice("https://finance.naver.com/item/main.naver?code=012330")
-            kospi["KaKaoPay"] = sharePrice("https://finance.naver.com/item/main.naver?code=377300")
-            kospi["SKInnovation"] = sharePrice("https://finance.naver.com/item/main.naver?code=096770")
-            Log.d("kospi", kospi.toString())
+            while (true) {
+                kospi["Samsung"] = sharePrice("https://finance.naver.com/item/main.naver?code=005930")
+                kospi["LGEnergySolution"] = sharePrice("https://finance.naver.com/item/main.naver?code=373220")
+                kospi["SKHynix"] = sharePrice("https://finance.naver.com/item/main.naver?code=000660")
+                kospi["NAVER"] = sharePrice("https://finance.naver.com/item/main.naver?code=035420")
+                kospi["SamsungBiologics"] = sharePrice("https://finance.naver.com/item/main.naver?code=207940")
+                kospi["KaKao"] = sharePrice("https://finance.naver.com/item/main.naver?code=035720")
+                kospi["Hyundai"] = sharePrice("https://finance.naver.com/item/main.naver?code=005380")
+                kospi["SamsungSDI"] = sharePrice("https://finance.naver.com/item/main.naver?code=006400")
+                kospi["LGChemistry"] = sharePrice("https://finance.naver.com/item/main.naver?code=051910")
+                kospi["Kia"] = sharePrice("https://finance.naver.com/item/main.naver?code=000270")
+                kospi["KakaoBank"] = sharePrice("https://finance.naver.com/item/main.naver?code=323410")
+                kospi["Celltrion"] = sharePrice("https://finance.naver.com/item/main.naver?code=068270")
+                kospi["POSCO"] = sharePrice("https://finance.naver.com/item/main.naver?code=005490")
+                kospi["KBFinancialGroup"] = sharePrice("https://finance.naver.com/item/main.naver?code=105560")
+                kospi["SamsungCnT"] = sharePrice("https://finance.naver.com/item/main.naver?code=028260")
+                kospi["LGElectronics"] = sharePrice("https://finance.naver.com/item/main.naver?code=066570")
+                kospi["ShinhanHoldings"] = sharePrice("https://finance.naver.com/item/main.naver?code=055550")
+                kospi["HyundaiMobis"] = sharePrice("https://finance.naver.com/item/main.naver?code=012330")
+                kospi["KaKaoPay"] = sharePrice("https://finance.naver.com/item/main.naver?code=377300")
+                kospi["SKInnovation"] = sharePrice("https://finance.naver.com/item/main.naver?code=096770")
+            }
         }
-
     }
-}
 
-suspend fun sharePrice(url: String): Int {
-    val price: Int = withContext(Dispatchers.IO) {
-        val doc: Document = Jsoup.connect(url).get()
-        val todayList = doc.select(".new_totalinfo dl>dd")
-        val data = todayList[3].text().split(" ")[1]
-        data.replace(",", "").toInt()
+    private suspend fun sharePrice(url: String): Int {
+        val price: Int = withContext(Dispatchers.IO) {
+            val doc: Document = Jsoup.connect(url).get()
+            val todayList = doc.select(".new_totalinfo dl>dd")
+            val data = todayList[3].text().split(" ")[1]
+            data.replace(",", "").toInt()
+        }
+        return price
     }
-    return price
 }
 
 
